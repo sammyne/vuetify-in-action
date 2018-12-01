e475f65d01fad6862a9e5d6bed5769dc57129ad6
sammy00 <xiangminli@outlook.com>
2018-12-01 22:16:46 +0800

    add row-and-column-breakpoints demo for grid-system

--- application-layout/grid-system/src/App.vue
+++ application-layout/grid-system/src/App.vue
@@ -5,6 +5,7 @@
       <offset/>
       <order/>
       <direction-and-align/>
+      <row-and-column-breakpoints/>
     </v-app>
   </div>
 </template>
@@ -13,6 +14,7 @@
 import DirectionAndAlign from "./components/DirectionAndAlign.vue";
 import Offset from "./components/Offset.vue";
 import Order from "./components/Order.vue";
+import RowAndColumnBreakpoints from "./components/RowAndColumnBreakpoints.vue";
 import Usage from "./components/Usage.vue";
 
 export default {
@@ -21,6 +23,7 @@ export default {
     DirectionAndAlign,
     Offset,
     Order,
+    RowAndColumnBreakpoints,
     Usage
   }
 };
---
+++ application-layout/grid-system/src/components/RowAndColumnBreakpoints.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container grid-list-xl>
+    <h3>Row And Column Breakpoints</h3>
+    <v-layout v-bind="binding">
+      <v-flex>
+        <v-card dark color="primary">
+          <v-card-text>one</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex>
+        <v-card dark color="secondary">
+          <v-card-text>two</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex>
+        <v-card dark color="accent">
+          <v-card-text>three</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
