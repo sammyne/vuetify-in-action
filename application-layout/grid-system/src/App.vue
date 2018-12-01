5e1fbd9dd22d8b0174fb97eea7888f6ab90a359a
sammy00 <xiangminli@outlook.com>
2018-12-01 22:06:06 +0800

    add the offset demo of grid-system

--- application-layout/grid-system/src/App.vue
+++ application-layout/grid-system/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <v-app>
       <usage/>
+      <offset/>
     </v-app>
   </div>
 </template>
 
 <script>
+import Offset from "./components/Offset.vue";
 import Usage from "./components/Usage.vue";
 
 export default {
   name: "app",
   components: {
+    Offset,
     Usage
   }
 };
