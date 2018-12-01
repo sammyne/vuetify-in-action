be7059b844dd4d6d6fa1b6924e0b0d6bcf56fbae
sammy00 <xiangminli@outlook.com>
2018-12-01 22:13:25 +0800

    add direction-and-align demo for grid-system

--- application-layout/grid-system/src/App.vue
+++ application-layout/grid-system/src/App.vue
@@ -4,11 +4,13 @@
       <usage/>
       <offset/>
       <order/>
+      <direction-and-align/>
     </v-app>
   </div>
 </template>
 
 <script>
+import DirectionAndAlign from "./components/DirectionAndAlign.vue";
 import Offset from "./components/Offset.vue";
 import Order from "./components/Order.vue";
 import Usage from "./components/Usage.vue";
@@ -16,6 +18,7 @@ import Usage from "./components/Usage.vue";
 export default {
   name: "app",
   components: {
+    DirectionAndAlign,
     Offset,
     Order,
     Usage
