2850a9277441c9453e96b5aba2f2d82974583a6c
sammy00 <xiangminli@outlook.com>
2018-12-01 22:01:08 +0800

    add the Usage demo for grid-system

---
+++ application-layout/grid-system/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <v-app>
+      <usage/>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import Usage from "./components/Usage.vue";
+
+export default {
+  name: "app",
+  components: {
+    Usage
+  }
+};
+</script>
