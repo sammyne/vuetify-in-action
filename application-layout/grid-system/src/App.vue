e7676715df2e1da56a505b75c73645f2e4114520
sammy00 <xiangminli@outlook.com>
2018-12-01 22:09:09 +0800

    add the order demo for grid-system

--- application-layout/grid-system/src/App.vue
+++ application-layout/grid-system/src/App.vue
@@ -3,18 +3,21 @@
     <v-app>
       <usage/>
       <offset/>
+      <order/>
     </v-app>
   </div>
 </template>
 
 <script>
 import Offset from "./components/Offset.vue";
+import Order from "./components/Order.vue";
 import Usage from "./components/Usage.vue";
 
 export default {
   name: "app",
   components: {
     Offset,
+    Order,
     Usage
   }
 };
