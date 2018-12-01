0f9b765a02f33bf6a6f07a1d49a9cc2934bf222d
sammy00 <xiangminli@outlook.com>
2018-12-01 21:26:59 +0800

    add demo of pre-defined-layouts

---
+++ application-layout/pre-defined-layouts/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <default-app-markup/>
+</template>
+
+<script>
+import DefaultAppMarkup from "./components/DefaultAppMarkup.vue";
+
+export default {
+  name: "app",
+  components: {
+    DefaultAppMarkup
+  }
+};
+</script>
