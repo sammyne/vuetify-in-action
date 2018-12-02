e730f8d856e38a422a9b37f46f53ec35be10ba6a
sammy00 <xiangminli@outlook.com>
2018-12-02 19:55:51 +0800

    demo bottom sheet

---
+++ ui-components/bottom-sheet/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <v-app>
+      <usage/>
+      <inset/>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import Inset from "./components/Inset";
+import Usage from "./components/Usage";
+
+export default {
+  name: "app",
+  components: {
+    Inset,
+    Usage
+  }
+};
+</script>
