5186295ed2a4c32236e0e1d587256039a0da529a
sammy00 <xiangminli@outlook.com>
2018-12-02 19:50:17 +0800

    demo bottom-nav

---
+++ ui-components/bottom-navigation/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-container>
+        <usage/>
+        <color-and-shift/>
+        <toggle/>
+      </v-container>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import ColorAndShift from "./components/ColorAndShift";
+import Toggle from "./components/Toggle";
+import Usage from "./components/Usage";
+
+export default {
+  name: "app",
+  components: {
+    ColorAndShift,
+    Toggle,
+    Usage
+  }
+};
+</script>
