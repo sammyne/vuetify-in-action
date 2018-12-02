52bb15d33f8dd729f76e8537dc1ab1ee71dafa06
sammy00 <xiangminli@outlook.com>
2018-12-02 21:12:33 +0800

    demo card

---
+++ ui-components/card/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <v-app>
+      <usage/>
+      <media-with-text/>
+      <horizontal-cards/>
+      <grids/>
+      <custom-actions/>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import CustomActions from "./components/CustomActions";
+import Grids from "./components/Grids";
+import HorizontalCards from "./components/HorizontalCards";
+import MediaWithText from "./components/MediaWithText";
+import Usage from "./components/Usage";
+
+export default {
+  name: "app",
+  components: {
+    CustomActions,
+    Grids,
+    HorizontalCards,
+    MediaWithText,
+    Usage
+  }
+};
+</script>
