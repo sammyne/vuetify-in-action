1211e5a623f94fa85b8563b4d5b81a6d73c9a91f
sammy00 <xiangminli@outlook.com>
2018-12-02 20:08:23 +0800

    demo breadcrumbs

---
+++ ui-components/breadcrumbs/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-container>
+        <usage/>
+        <icon-dividers/>
+        <item-slot/>
+      </v-container>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import IconDividers from "./components/IconDividers";
+import ItemSlot from "./components/ItemSlot";
+import Usage from "./components/Usage";
+
+export default {
+  name: "app",
+  components: {
+    IconDividers,
+    ItemSlot,
+    Usage
+  }
+};
+</script>
