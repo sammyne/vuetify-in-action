307b477f32bf609b07108d976c121e3a69ef42c5
sammy00 <xiangminli@outlook.com>
2018-12-02 14:32:12 +0800

    demo motion

---
+++ motion-and-transitions/motion/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <v-app>
+      <slide-x/>
+      <slide-y/>
+      <scale/>
+      <fade/>
+      <custom-origin/>
+      <todo-list/>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import CustomOrigin from "./components/CustomOrigin";
+import Fade from "./components/Fade";
+import Scale from "./components/Scale";
+import SlideX from "./components/SlideX";
+import SlideY from "./components/SlideY";
+import TodoList from "./components/TodoList";
+
+export default {
+  name: "app",
+  components: {
+    CustomOrigin,
+    Fade,
+    Scale,
+    SlideX,
+    SlideY,
+    TodoList
+  }
+};
+</script>
