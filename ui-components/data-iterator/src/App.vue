bf830229e6c7b50bb63026259ac56913b25e1eba
sammy00 <xiangminli@outlook.com>
2018-12-02 21:45:03 +0800

    demo data-iterator

---
+++ ui-components/data-iterator/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-container>
+        <usage/>
+        <slots/>
+      </v-container>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import Slots from "./components/Slots";
+import Usage from "./components/Usage";
+
+export default {
+  name: "app",
+  components: {
+    Slots,
+    Usage
+  }
+};
+</script>
