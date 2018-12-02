ea2cb4187351621a925eb4e953016188965d987e
sammy00 <xiangminli@outlook.com>
2018-12-02 10:29:00 +0800

    demo elevation

---
+++ application-layout/elevation/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-container grid-list-md text-xs-center>
+        <v-layout row wrap>
+          <v-flex xs12 md6>
+            <v-card>
+              <v-card-text>No Elevation</v-card-text>
+            </v-card>
+          </v-flex>
+          <v-flex v-for="level in levels" v-bind:key="level" xs12 md6>
+            <v-card :class="level">
+              <v-card-text>{{ level }}</v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      levels: [...new Array(25).keys()].map(item => `elevation-${item}`)
+    };
+  }
+};
+</script>
