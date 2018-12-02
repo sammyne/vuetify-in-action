498c0395294be021f419ac4e4b386836cdd56d4f
sammy00 <xiangminli@outlook.com>
2018-12-02 15:23:26 +0800

    demo avatar

---
+++ ui-components/avatar/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <v-app>
+      <usage/>
+      <advanced-usage/>
+      <icon-and-text/>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import AdvancedUsage from "./components/AdvancedUsage";
+import IconAndText from "./components/IconAndText";
+import Usage from "./components/Usage";
+
+export default {
+  name: "app",
+  components: {
+    AdvancedUsage,
+    IconAndText,
+    Usage
+  }
+};
+</script>
