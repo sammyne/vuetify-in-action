cc2f7896f0b7993eeadac8ca7c255618a156679f
sammy00 <xiangminli@outlook.com>
2018-12-03 08:32:33 +0800

    dividers

---
+++ ui-components/dividers/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-container>
+        <usage/>
+        <light-and-dark/>
+        <inset-dividers/>
+        <subheaders-and-dividers/>
+        <list-dividers/>
+        <dividers-in-portrait-view/>
+        <vertical-dividers/>
+      </v-container>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import DividersInPortraitView from "./components/DividersInPortraitView";
+import InsetDividers from "./components/InsetDividers";
+import LightAndDark from "./components/LightAndDark";
+import ListDividers from "./components/ListDividers";
+import SubheadersAndDividers from "./components/SubheadersAndDividers";
+import Usage from "./components/Usage";
+import VerticalDividers from "./components/VerticalDividers";
+
+export default {
+  name: "app",
+  components: {
+    DividersInPortraitView,
+    InsetDividers,
+    LightAndDark,
+    ListDividers,
+    SubheadersAndDividers,
+    Usage,
+    VerticalDividers
+  }
+};
+</script>
