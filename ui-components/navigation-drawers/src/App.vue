66e376edc592ac9e0f7d1285a624810d4070ee9a
sammy00 <xiangminli@outlook.com>
2018-12-03 09:24:42 +0800

    nav-drawers

---
+++ ui-components/navigation-drawers/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <v-app>
+      <!--
+      <usage/>
+      <colored-drawer/>
+      <permanent-floating-drawer/>
+      <avatars/>
+      -->
+      <!--<combined-drawers/>-->
+      <!--<dark-theme/>-->
+      <!--<mini/>-->
+      <!--<nested-lists/>-->
+      <temporary/>
+    </v-app>
+  </div>
+</template>
+
+<script>
+/*
+import Avatars from "./components/Avatars";
+import ColoredDrawer from "./components/ColoredDrawer";
+import PermanentFloatingDrawer from "./components/PermanentFloatingDrawer";
+import Usage from "./components/Usage";
+*/
+
+//import CombinedDrawers from "./components/CombinedDrawers";
+//import DarkTheme from "./components/DarkTheme";
+//import Mini from "./components/Mini";
+//import NestedLists from "./components/NestedLists";
+import Temporary from "./components/Temporary";
+
+export default {
+  name: "app",
+  components: {
+    /*
+    Avatars,
+    ColoredDrawer,
+    PermanentFloatingDrawer,
+    Usage
+    */
+
+    //CombinedDrawers
+    //DarkTheme
+    //Mini
+    //NestedLists
+    Temporary
+  }
+};
+</script>
---
+++ ui-components/navigation-drawers/src/components/ColoredDrawer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Colored Drawer</h1>
+    <v-navigation-drawer class="blue lighten-3" dark permanent>
+      <v-list>
+        <v-list-tile v-for="item in items" :key="item.title" @click>
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { title: "Dashboard", icon: "dashboard" },
+        { title: "Account", icon: "account_box" },
+        { title: "Admin", icon: "gavel" }
+      ]
+    };
+  }
+};
+</script>
