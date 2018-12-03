5602061f6e9f86992ceb4b50147baa72f851f4f6
sammy00 <xiangminli@outlook.com>
2018-12-03 08:42:17 +0800

    footer

---
+++ ui-components/footer/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-container>
+        <usage/>
+        <company-footer/>
+        <indigo-footer/>
+        <teal-footer/>
+      </v-container>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import CompanyFooter from "./components/CompanyFooter";
+import IndigoFooter from "./components/IndigoFooter";
+import TealFooter from "./components/TealFooter";
+import Usage from "./components/Usage";
+
+export default {
+  name: "app",
+  components: {
+    CompanyFooter,
+    IndigoFooter,
+    TealFooter,
+    Usage
+  }
+};
+</script>
---
+++ ui-components/footer/src/components/Usage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Usage</h1>
+    <v-footer class="pa-3">
+      <v-spacer></v-spacer>
+      <div>&copy; {{ new Date().getFullYear() }}</div>
+    </v-footer>
+  </div>
+</template>
