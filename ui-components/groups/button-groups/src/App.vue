53b701a9b5d40a5d66283a808ccb7593ca7d4b4a
sammy00 <xiangminli@outlook.com>
2018-12-05 13:24:36 +0800

    two UI-components about grouping

---
+++ ui-components/groups/button-groups/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-container>
+        <usage/>
+        <in-toolbar/>
+      </v-container>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import InToolbar from "./components/InToolbar";
+import Usage from "./components/Usage";
+
+export default {
+  name: "app",
+  components: {
+    InToolbar,
+    Usage
+  }
+};
+</script>
---
+++ ui-components/groups/item-groups/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <v-app>
+      <usage/>
+      <chips/>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import Chips from "./components/Chips";
+import Usage from "./components/Usage";
+
+export default {
+  name: "app",
+  components: {
+    Chips,
+    Usage
+  }
+};
+</script>
