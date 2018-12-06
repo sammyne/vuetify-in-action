776645e4ce615fdb42e0d9b70b897f1b1656641f
sammy00 <xiangminli@outlook.com>
2018-12-06 12:54:16 +0800

    add on autocompletes

---
+++ ui-components/inputs-and-controls/autocompletes/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-container>
+        <usage/>
+        <searching-an-api/>
+        <scoped-slots/>
+        <custom-filter/>
+        <asynchronous-items/>
+        <advanced-slots/>
+      </v-container>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import AdvancedSlots from "./components/AdvancedSlots";
+import AsynchronousItems from "./components/AsynchronousItems";
+import CustomFilter from "./components/CustomFilter";
+import ScopedSlots from "./components/ScopedSlots";
+import SearchingAnApi from "./components/SearchingAnAPI";
+import Usage from "./components/Usage";
+
+export default {
+  name: "app",
+  components: {
+    AdvancedSlots,
+    AsynchronousItems,
+    CustomFilter,
+    ScopedSlots,
+    SearchingAnApi,
+    Usage
+  }
+};
+</script>
