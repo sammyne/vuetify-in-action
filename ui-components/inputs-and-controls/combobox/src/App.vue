6a5533d81817f2ed5d00e0f04d469d5fd5a9e3bf
sammy00 <xiangminli@outlook.com>
2018-12-07 13:48:44 +0800

    demo combobox

---
+++ ui-components/inputs-and-controls/combobox/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <v-app>
+      <usage/>
+      <no-data-with-chips/>
+      <advanced-custom-options/>
+      <multiple-combobox/>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import AdvancedCustomOptions from "./components/AdvancedCustomOptions";
+import MultipleCombobox from "./components/MultipleCombobox";
+import NoDataWithChips from "./components/NoDataWithChips";
+import Usage from "./components/Usage";
+
+export default {
+  name: "app",
+  components: {
+    AdvancedCustomOptions,
+    MultipleCombobox,
+    NoDataWithChips,
+    Usage
+  }
+};
+</script>
