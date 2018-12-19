115e98bb224e449d13d660901a173cb2741f2de3
sammy00 <xiangminli@outlook.com>
2018-12-19 19:13:23 +0800

    demo focusable

---
+++ ui-components/expansion-panels/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <v-app>
+      <usage/>
+      <expand/>
+      <expand-control/>
+      <disabled-and-readonly/>
+      <popout-inset/>
+      <custom-icon/>
+      <focusable/>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import CustomIcon from "./components/CustomIcon";
+import DisabledAndReadonly from "./components/DisabledAndReadonly";
+import Expand from "./components/Expand";
+import Focusable from "./components/Focusable";
+import ExpandControl from "./components/ExpandControl";
+import PopoutInset from "./components/PopoutInset";
+import Usage from "./components/Usage";
+
+export default {
+  name: "app",
+  components: {
+    CustomIcon,
+    DisabledAndReadonly,
+    Expand,
+    ExpandControl,
+    Focusable,
+    PopoutInset,
+    Usage
+  }
+};
+</script>
