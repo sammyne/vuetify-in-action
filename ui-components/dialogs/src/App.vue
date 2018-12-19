6d533a793f662d5f556bddeb6f24e2b57b57cbbc
sammy00 <xiangminli@outlook.com>
2018-12-19 18:57:51 +0800

    demo dialogs

---
+++ ui-components/dialogs/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <v-app>
+      <usage/>
+      <without-activator/>
+      <modal/>
+      <fullscreen/>
+      <my-form/>
+      <scrollable/>
+      <overflowed/>
+      <simple-dialogs/>
+      <loader/>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import Fullscreen from "./components/Fullscreen";
+import Loader from "./components/Loader";
+import Modal from "./components/Modal";
+import MyForm from "./components/Form";
+import Overflowed from "./components/Overflowed";
+import Scrollable from "./components/Scrollable";
+import SimpleDialogs from "./components/SimpleDialogs";
+import Usage from "./components/Usage";
+import WithoutActivator from "./components/WithoutActivator";
+
+export default {
+  name: "app",
+  components: {
+    Fullscreen,
+    Loader,
+    Modal,
+    MyForm,
+    Overflowed,
+    Scrollable,
+    SimpleDialogs,
+    Usage,
+    WithoutActivator
+  }
+};
+</script>
---
+++ ui-components/dialogs/src/components/WithoutActivator.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <h1 class="text-sm-center">Without Activator</h1>
+    <v-layout row wrap justify-center>
+      <v-btn color="primary" class="text-sm-center" dark @click="dialog = true">Open Dialog</v-btn>
+
+      <v-dialog v-model="dialog" max-width="290">
+        <v-card>
+          <v-card-title class="headline">Use Google's location service?</v-card-title>
+
+          <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn color="green darken-1" flat="flat" @click="dialog = false">Disagree</v-btn>
+
+            <v-btn color="green darken-1" flat="flat" @click="dialog = false">Agree</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false
+    };
+  }
+};
+</script>
