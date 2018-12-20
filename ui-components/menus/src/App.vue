1390fcce11ba91db15aa3444632bb38e4cb9943f
sammy00 <xiangminli@outlook.com>
2018-12-20 10:31:45 +0800

    demo menus

---
+++ ui-components/menus/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <v-app>
+      <usage/>
+      <absolute-position/>
+      <absolute-position-without-activator/>
+      <hover/>
+      <menus/>
+      <custom-transitions/>
+      <popover-menu/>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import AbsolutePosition from "./components/AbsolutePosition";
+import AbsolutePositionWithoutActivator from "./components/AbsolutePositionWithoutActivator";
+import CustomTransitions from "./components/CustomTransitions";
+import Hover from "./components/Hover";
+import Menus from "./components/Menus";
+import PopoverMenu from "./components/PopoverMenu";
+import Usage from "./components/Usage";
+
+export default {
+  name: "app",
+  components: {
+    AbsolutePosition,
+    AbsolutePositionWithoutActivator,
+    CustomTransitions,
+    Hover,
+    Menus,
+    PopoverMenu,
+    Usage
+  }
+};
+</script>
---
+++ ui-components/menus/src/components/AbsolutePositionWithoutActivator.vue
@@ -0,0 +1,58 @@
+<!-- eslint-disable -->
+<template>
+  <v-container>
+    <h1>Absolute Position without Activator</h1>
+    <v-layout flex justify-center>
+      <v-card
+        class="portrait"
+        img="https://cdn.vuetifyjs.com/images/cards/girl.jpg"
+        height="300px"
+        @contextmenu="show"
+      ></v-card>
+    </v-layout>
+
+    <v-menu v-model="showMenu" :position-x="x" :position-y="y" absolute offset-y>
+      <v-list>
+        <v-list-tile v-for="(item, index) in items" :key="index" @click>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    showMenu: false,
+    x: 0,
+    y: 0,
+    items: [
+      { title: "Click Me" },
+      { title: "Click Me" },
+      { title: "Click Me" },
+      { title: "Click Me 2" }
+    ]
+  }),
+
+  methods: {
+    show(e) {
+      e.preventDefault();
+      this.showMenu = false;
+      this.x = e.clientX;
+      this.y = e.clientY;
+      this.$nextTick(() => {
+        this.showMenu = true;
+      });
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped>
+.portrait.v-card {
+  margin: 0 auto;
+  max-width: 600px;
+  width: 100%;
+}
+</style>
