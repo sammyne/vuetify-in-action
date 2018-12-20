e93b229a0b6c3c05826a63529dc6b9dcf4295069
sammy00 <xiangminli@outlook.com>
2018-12-20 09:43:47 +0800

    demo transitions

---
+++ ui-components/hover/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <v-app>
+      <usage/>
+      <transitions/>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import Transitions from "./components/Transitions";
+import Usage from "./components/Usage";
+
+export default {
+  name: "app",
+  components: {
+    Transitions,
+    Usage
+  }
+};
+</script>
