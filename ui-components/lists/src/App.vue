9d1aa1f1f1f79249b15d0e46bcd59a5d93ec326e
sammy00 <xiangminli@outlook.com>
2018-12-20 10:58:28 +0800

    demo lists

---
+++ ui-components/lists/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <v-app>
+      <usage/>
+      <avatar-with-title-and-action/>
+      <icon-with2-lines-and-action/>
+      <avatar-with3-lines/>
+      <avatar-with-title-and-action2/>
+      <subheadings-and-dividers/>
+      <card-image-with-toolbar-and-list/>
+      <title-with-subtitle-actions-and-action-text/>
+      <action-with-title-and-sub-title/>
+      <expansion-lists/>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import ActionWithTitleAndSubTitle from "./components/ActionWithTitleAndSubTitle";
+import AvatarWith3Lines from "./components/AvatarWith3Lines";
+import AvatarWithTitleAndAction from "./components/AvatarWithTitleAndAction";
+import AvatarWithTitleAndAction2 from "./components/AvatarWithTitleAndAction2";
+import CardImageWithToolbarAndList from "./components/CardImageWithToolbarAndList";
+import ExpansionLists from "./components/ExpansionLists";
+import IconWith2LinesAndAction from "./components/IconWith2LinesAndAction";
+import SubheadingsAndDividers from "./components/SubheadingsAndDividers";
+import Usage from "./components/Usage";
+import TitleWithSubtitleActionsAndActionText from "./components/TitleWithSubtitleActionsAndActionText";
+
+export default {
+  name: "app",
+  components: {
+    ActionWithTitleAndSubTitle,
+    AvatarWithTitleAndAction,
+    AvatarWithTitleAndAction2,
+    AvatarWith3Lines,
+    CardImageWithToolbarAndList,
+    ExpansionLists,
+    IconWith2LinesAndAction,
+    SubheadingsAndDividers,
+    TitleWithSubtitleActionsAndActionText,
+    Usage
+  }
+};
+</script>
