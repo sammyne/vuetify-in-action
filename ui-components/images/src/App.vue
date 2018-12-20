eb7011d1252b02878daa7e38270ec7f6d1c7f2be
sammy00 <xiangminli@outlook.com>
2018-12-20 10:17:38 +0800

    demo images

---
+++ ui-components/images/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <v-app>
+      <usage/>
+      <contain-and-cover/>
+      <height/>
+      <fixed-ratio/>
+      <gradients/>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import ContainAndCover from "./components/ContainAndCover.vue";
+import FixedRatio from "./components/FixedRatio.vue";
+import Gradients from "./components/Gradients.vue";
+import Height from "./components/Height.vue";
+import Usage from "./components/Usage.vue";
+
+export default {
+  name: "app",
+  components: {
+    ContainAndCover,
+    FixedRatio,
+    Gradients,
+    Height,
+    Usage
+  }
+};
+</script>
---
+++ ui-components/images/src/components/ContainAndCover.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <h1>Contain and Cover</h1>
+    <v-layout justify-space-around>
+      <v-flex xs5>
+        <div class="title mb-1">Default (cover)</div>
+        <v-layout column>
+          <div class="subheading">Matching</div>
+          <v-img src="https://picsum.photos/510/300?random" aspect-ratio="1.7"></v-img>
+          <div class="subheading pt-3">Too high</div>
+          <v-img src="https://picsum.photos/510/300?random" aspect-ratio="2"></v-img>
+          <div class="subheading pt-3">Too low</div>
+          <v-img src="https://picsum.photos/510/300?random" aspect-ratio="1.4"></v-img>
+        </v-layout>
+      </v-flex>
+
+      <v-flex xs5>
+        <div class="title mb-1">Contain</div>
+        <v-layout column>
+          <div class="subheading">Matching</div>
+          <v-img src="https://picsum.photos/510/300?random" aspect-ratio="1.7" contain></v-img>
+          <div class="subheading pt-3">Too high</div>
+          <v-img src="https://picsum.photos/510/300?random" aspect-ratio="2" contain></v-img>
+          <div class="subheading pt-3">Too low</div>
+          <v-img src="https://picsum.photos/510/300?random" aspect-ratio="1.4" contain></v-img>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
