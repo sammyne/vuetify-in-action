71aad01df9f90beaa574d220fd249418fe82f17c
Sammy <xiangmin.li@qq.com>
2019-01-21 13:57:48 +0800

    demo the alternate labels of steppers

--- ui-components/steppers/src/App.vue
+++ ui-components/steppers/src/App.vue
@@ -9,6 +9,9 @@
           <optionals/>
           <horizontal/>
           <vertical/>
+          <linear/>
+          <non-linear/>
+          <alternate-labels/>
         </v-content>
       </v-container>
     </v-app>
@@ -16,20 +19,26 @@
 </template>
 
 <script>
+import AlternateLabels from "./components/AlternateLabels";
 import EditableSteps from "./components/EditableSteps";
 import HelloWorld from "./components/HelloWorld.vue";
 import Horizontal from "./components/Horizontal";
+import Linear from "./components/Linear";
 import NonEditableSteps from "./components/NonEditableSteps";
+import NonLinear from "./components/NonLinear";
 import Optionals from "./components/Optionals";
 import Vertical from "./components/Vertical";
 
 export default {
   name: "app",
   components: {
+    AlternateLabels,
     EditableSteps,
     HelloWorld,
     Horizontal,
+    Linear,
     NonEditableSteps,
+    NonLinear,
     Optionals,
     Vertical
   }
---
+++ ui-components/steppers/src/components/NonLinear.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-layout row wrap>
+    <v-flex sm12>
+      <h1>Non-linear steppers</h1>
+    </v-flex>
+    <v-flex sm12>
+      <v-stepper non-linear>
+        <v-stepper-header>
+          <v-stepper-step editable step="1">Select campaign settings</v-stepper-step>
+
+          <v-divider></v-divider>
+
+          <v-stepper-step editable step="2">Create an ad group</v-stepper-step>
+
+          <v-divider></v-divider>
+
+          <v-stepper-step step="3" editable>Create an ad</v-stepper-step>
+        </v-stepper-header>
+      </v-stepper>
+
+      <v-stepper class="mt-5" non-linear>
+        <v-stepper-header>
+          <v-stepper-step complete editable step="1">Select campaign settings</v-stepper-step>
+
+          <v-divider></v-divider>
+
+          <v-stepper-step editable step="2">Create an ad group</v-stepper-step>
+
+          <v-divider></v-divider>
+
+          <v-stepper-step complete editable step="3">Create an ad</v-stepper-step>
+        </v-stepper-header>
+      </v-stepper>
+
+      <v-stepper class="mt-5" non-linear value="3">
+        <v-stepper-header>
+          <v-stepper-step complete editable step="1">Select campaign settings</v-stepper-step>
+
+          <v-divider></v-divider>
+
+          <v-stepper-step complete editable step="2">Create an ad group</v-stepper-step>
+
+          <v-divider></v-divider>
+
+          <v-stepper-step complete editable step="3">Create an ad</v-stepper-step>
+        </v-stepper-header>
+      </v-stepper>
+    </v-flex>
+  </v-layout>
+</template>
