2fa0a8620de68fdcaea7719414a629fc05dcd806
Sammy <lixiangmin01@baidu.com>
2019-08-31 15:06:23 +0800

    feature(styles-and-animations/display): a basic demo

---
+++ styles-and-animations/display/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs12>
+          <div>
+            <div class="d-inline pa-2 deep-purple accent-4 white--text">d-inline</div>
+            <div class="d-inline pa-2 black white--text">d-inline</div>
+          </div>
+        </v-flex>
+        <v-flex xs12>
+          <div>
+            <div class="d-block pa-2 deep-purple accent-4 white--text">d-block</div>
+            <div class="d-block pa-2 black white--text">d-block</div>
+          </div>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "app"
+};
+</script>
