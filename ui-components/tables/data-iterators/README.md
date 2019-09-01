f62ea23ade8dab114e5106a34805d5f82e82a252
Sammy <lixiangmin01@baidu.com>
2019-09-01 14:54:03 +0800

    feature(ui-components/tables/data-iterators): upgrade to v2

--- ui-components/tables/data-iterators/README.md
+++ ui-components/tables/data-iterators/README.md
@@ -1,4 +1,4 @@
-# data-iterator
+# display
 
 ## Project setup
 ```
--- ui-components/tables/data-iterators/src/App.vue
+++ ui-components/tables/data-iterators/src/App.vue
@@ -2,20 +2,26 @@
   <div id="app">
     <v-app>
       <v-container>
-        <usage/>
-        <slots/>
+        <usage />
+        <slots />
+        <expand />
+        <filter-demo />
       </v-container>
     </v-app>
   </div>
 </template>
 
 <script>
+import Expand from "./components/Expand";
+import FilterDemo from "./components/Filter";
 import Slots from "./components/Slots";
 import Usage from "./components/Usage";
 
 export default {
   name: "app",
   components: {
+    Expand,
+    FilterDemo,
     Slots,
     Usage
   }
